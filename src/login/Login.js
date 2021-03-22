90a777595260d46de59bb619a084e6c8520390b3
Bruce Gordon <brc.gordon@gmail.com>
2021-03-22 16:05:37 -0700

    Create Login component

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,10 +1,41 @@
 import './Login.css';
 import { useState } from 'react';
 
-const Login = () => {
+const Login = ({ submit }) => {
+
+  const [loginValue, setLoginValue] = useState('');
+
+  const handleChange = (e) => {
+    setLoginValue(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submit(loginValue)
+  }
+
+  const checkForInput = () => {
+    return (loginValue) ? false : true
+  }
+
   return (
-    <p>Hi!</p>
-  )
+    <div className='login'>
+        <h1>San Junipero University</h1>
+        <h2>Student Course Regristration</h2>
+        <input
+          className='login-field'
+          type='text'
+          value={ loginValue }
+          onChange={ handleChange }
+          placeholder='Enter id here'>
+        </input>
+        <button
+          onClick={ handleSubmit }
+          disabled={ checkForInput() }
+          className='login-button'>Log In
+        </button>
+      </div>
+  );
 }
 
 export default Login;
