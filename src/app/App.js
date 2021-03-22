c252e8c89f32a96987a2d1eb69fe213e47f83994
Bruce Gordon <brc.gordon@gmail.com>
2021-03-22 16:01:45 -0700

    Add Login

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,13 @@ import { Route, Switch, Link, NavLink } from 'react-router-dom';
 import Login from '../login/Login.js';
 import Catalog from '../catalog/Catalog.js';
 import Schedule from '../schedule/Schedule.js';
+import {
+  getAllCourses,
+  getUser,
+  getUserEnrollments,
+  createEnrollment,
+  deleteEnrollment
+} from '../apiCalls';
 
 
 const App = () => {
@@ -14,8 +21,30 @@ const App = () => {
   const [error, setError] = useState('');
 
   useEffect(() => {
-
+    getAllCourses()
+      .then(resp => setCourses(resp.courses))
   }, [])
+
+  const submitLogin = async (userId) => {
+    await getUser(userId)
+      .then(resp => setUser(resp))
+    getUserEnrollments(userId)
+      .then(resp => setEnrollments(resp))
+  }
+
+  const checkError = () => {
+    if (user.message) {
+      return (
+        <p>
+          No user with that id was found.  Please try again.
+        </p>
+      )
+    }
+  }
+
+  const logoutUser = () => {
+    setUser({});
+  }
 
   if (!user.id) {
     return (
---
+++ src/login/Login.js
@@ -0,0 +1,10 @@
+import './Login.css';
+import { useState } from 'react';
+
+const Login = () => {
+  return (
+    <p>Hi!</p>
+  )
+}
+
+export default Login;
